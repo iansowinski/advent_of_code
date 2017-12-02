a16fee070bd649cc931fb9c0ff7336e43073ffd8
Ian Sowinski <sowinj@gmail.com>
2017-12-02 14:55:42 +0100

    formatted

--- 1-1.go
+++ 1-1.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "strconv"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-func main () {
+func main() {
 	input := "" //Here put the input
-  inputArray := strings.Split(input, "")
-  lastElementIndex := len(inputArray) - 1
-  result := 0
-  for index, number := range inputArray {
-    if index != lastElementIndex {
-      if number == inputArray[index + 1] {
-        numberInt, _ := strconv.Atoi(number)
-        result += numberInt
-      }
-    } else {
-      if number == inputArray[0] {
-        numberInt, _ := strconv.Atoi(number)
-        result += numberInt
-      }
-    }
-  }
-  fmt.Println(result)
+	inputArray := strings.Split(input, "")
+	lastElementIndex := len(inputArray) - 1
+	result := 0
+	for index, number := range inputArray {
+		if index != lastElementIndex {
+			if number == inputArray[index+1] {
+				numberInt, _ := strconv.Atoi(number)
+				result += numberInt
+			}
+		} else {
+			if number == inputArray[0] {
+				numberInt, _ := strconv.Atoi(number)
+				result += numberInt
+			}
+		}
+	}
+	fmt.Println(result)
 }
--- 2-1.go
+++ 2-1.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "strings"
-  "strconv"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-func main () {
+func main() {
 	input := "" //Here put the input
-  inputTempArray := strings.Split(input, "\n")
-  lengthTempArray := len(inputTempArray)
-  inputArray := make([][]int, lengthTempArray)
-  for index, numbers := range inputTempArray {
-  	tempStringsArray := strings.Split(numbers, "\t")
-  	for _, stringNumber := range tempStringsArray{
-  		intNumber, _ := strconv.Atoi(stringNumber)
-  		inputArray[index] = append(inputArray[index], intNumber)
-  	}
-  }
-  result := 0
-  for _, array := range inputArray {
-  	min, max := minMax(array)
-  	result = result + (max - min)
-  }
-  fmt.Println(result)
+	inputTempArray := strings.Split(input, "\n")
+	lengthTempArray := len(inputTempArray)
+	inputArray := make([][]int, lengthTempArray)
+	for index, numbers := range inputTempArray {
+		tempStringsArray := strings.Split(numbers, "\t")
+		for _, stringNumber := range tempStringsArray {
+			intNumber, _ := strconv.Atoi(stringNumber)
+			inputArray[index] = append(inputArray[index], intNumber)
+		}
+	}
+	result := 0
+	for _, array := range inputArray {
+		min, max := minMax(array)
+		result = result + (max - min)
+	}
+	fmt.Println(result)
 }
 
-
-func minMax(array []int)(int, int){
+func minMax(array []int) (int, int) {
 	var max int = array[0]
 	var min int = array[0]
 	for _, number := range array {
