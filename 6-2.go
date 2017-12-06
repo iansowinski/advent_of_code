57367576ecd3243cf489c4bf00775fd43444b20d
Ian Sowinski <sowinj@gmail.com>
2017-12-06 21:55:01 +0100

    day 6 solved

---
+++ 6-2.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+func main() {
+	input := "4	1	15	12	0	9	9	5	5	8	7	3	14	5	12	3" //Here put the input
+	// input := "0	2	7	0"
+	tempBanks := strings.Split(input, "\t")
+	banks := make([]int, len(tempBanks))
+	for index, item := range tempBanks {
+		banks[index], _ = strconv.Atoi(item)
+	}
+	banksHistory := make([]string, 0)
+	i := 0
+	
+	for {
+		if checkDuplicates(banksHistory, join(banks), i) {
+			fmt.Println(giveDuplicatesIndexes(banksHistory, join(banks), i))
+			break
+		}
+		banksHistory = append(banksHistory, join(banks))
+		i += 1
+		maxIndex, maxNumber := getMax(banks)
+		banks[maxIndex] = 0
+		for j := 0; j < maxNumber; j++ {
+			banks[(maxIndex + j + 1) % len(banks)] += 1
+		}
+	}
+}
+
+func getMax(array []int) (int, int) {
+	var max int = array[0]
+	var maxIndex int = 0
+	for index, number := range array {
+		if number > max {
+			max = number
+			maxIndex = index
+		}
+	}
+	return maxIndex, max
+}
+
+func checkDuplicates(array []string, itemToCheck string, itemToCheckIndex int) bool {
+	for index, number := range array {
+		if index != itemToCheckIndex && number == itemToCheck && number != "" {
+			return true
+		}
+	}
+	return false
+}
+
+func giveDuplicatesIndexes(array []string, itemToCheck string, itemToCheckIndex int) int {
+	for index, number := range array {
+		if index != itemToCheckIndex && number == itemToCheck && number != "" {
+			return itemToCheckIndex - index
+		}
+	}
+	return 0
+}
+
+func join (array []int) string {
+	return strings.Trim(strings.Join(strings.Split(fmt.Sprint(array), " "), ","), "[]")
+}
