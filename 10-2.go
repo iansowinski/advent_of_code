4d019c4fa80b336177a0e5b58da69349eb4e4841
Ian Sowinski <sowinj@gmail.com>
2017-12-10 17:46:24 +0100

    works for examples, but not for given input

---
+++ 10-2.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	// input := "" //Here put the input
+	array := generateArray(256)
+	input := "94,84,0,79,2,27,81,1,123,93,218,23,103,255,254,243"
+	byteArrayTemp := []rune(input)
+	toAdd := []rune{17, 31, 73, 47, 23}
+	byteArrayTemp = append(byteArrayTemp, toAdd...)
+	lengths := make([]int, len(byteArrayTemp))
+	for index, item := range byteArrayTemp {
+		lengths[index] = int(item)
+	}
+	marker := 0
+	skip := 0
+	i := 0
+	for i < 64 {
+		array, marker, skip = generateHash(lengths, array, marker, skip)
+		i += 1
+	}
+	sparseHash := createSparseHash(array)
+	denseHash := make([]string, 16)
+
+	for index, item := range sparseHash {
+		current := 0
+		for _, childItem := range item {
+			current = current ^ childItem
+		}
+		denseHash[index] = strconv.FormatInt(int64(current), 16)
+	}
+	fmt.Println(strings.Join(denseHash, ""))
+}
+
+func createSparseHash(array []int) [][]int {
+	sparseHash := make([][]int, 16)
+	i := 0
+	for _, item := range array {
+		sparseHash[i / 16] = append(sparseHash[i / 16], item)
+		i += 1
+	}
+	return sparseHash
+}
+
+func recursiveMarkerCheck (marker int, length int) int {
+	if marker > length {
+		marker = recursiveMarkerCheck(marker - length, length)
+	}
+	return marker
+}
+
+func generateHash(lengths []int, array []int, marker int, skip int) ([]int, int, int) {
+	for _, length := range lengths {
+		var tempArray []int
+		marker = recursiveMarkerCheck(marker, len(array))
+		if marker + length < len(array) {
+			tempArray = reverse(array[marker:(marker + length)])
+			array = replace(marker, array, tempArray)
+			marker =  incresePosition(marker, length, skip, len(array))
+			skip += 1
+		} else {
+			leftSide := array[(marker):]
+			rightSide := array[:(length - (len(array) - marker ))]
+			tempArray = append(leftSide, rightSide...)
+			array = replace(marker, array, reverse(tempArray))
+			marker = incresePosition(marker, length, skip, len(array))
+			skip += 1
+		} 
+	}
+	return array, marker, skip
+}
+
+func generateArray (size int) []int {
+	array := make([]int, size)
+	i := 0
+	for i < size {
+		array[i] = i
+		i += 1
+	}
+	return array
+}
+
+func incresePosition (marker int, length int, skip int, arrayLen int) int {
+	if marker + length + skip < arrayLen {
+		return marker + length + skip
+	}
+	return  marker + length + skip - arrayLen
+}
+
+func reverse (array []int) []int {
+	for i, j := 0, len(array)-1; i < j; i, j = i+1, j-1 {
+		array[i], array[j] = array[j], array[i]
+	}
+	return array
+}
+
+func replace (start int, array []int, reversedFragment []int) []int {
+	for index, item := range reversedFragment {
+		if index + start < len(array) {
+			array[index + start] = item
+		} else {
+			array[(index + start) - len(array)] = item
+		}
+	}
+	return array
+}
